87cb2394c3034d84a69c08db9be786db9240b9c6
shueibyare <shueibyussuf10@gmail.com>
2025-05-13 21:14:57 +0300

    added changes to new branch

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -1,3 +1,6 @@
+import random
+import string
+from django.contrib.auth.hashers import make_password
 from rest_framework.serializers import ModelSerializer
 from .models import User
 
@@ -5,3 +8,14 @@ class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = "__all__"
+
+    def create(self, validated_data):
+        request = self.context.get("request")
+        alphabets = string.ascii_letters
+        plain_password = ''.join(random.choices(alphabets, k=10))
+        validated_data["password"] = make_password(plain_password)
+        print(plain_password)
+
+        # TODO: add functionality to email user the Login credentials(username/email, plain_password) upon account creation
+
+        return super().create(validated_data)
--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import UserViewSet
+from .views import UserViewSet, sign_in
 
 router = routers.DefaultRouter()
 router.register("users", UserViewSet)
 
 urlpatterns = [
-    path("", include(router.urls))
+    path("", include(router.urls)),
+    path("login", sign_in)
 ]
